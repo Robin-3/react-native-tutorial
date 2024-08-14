e10290f9d1cade12fa350cbe5ac8e1dc9f3ac7db
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-14 22:27:42 +0000

    Sección 2: Custom Hook - useCounter

--- 01-react-foundation/src/App.tsx
+++ 01-react-foundation/src/App.tsx
@@ -1,11 +1,14 @@
-import { Counter } from "./components";
+import {
+  // Counter,
+  CounterWithHook
+} from "./components";
 // import {BasicFunctions, BasicTypes, ObjectLiterals} from "./typescript";
 
 function App() {
   return (
     <main>
       <h1>Introducción a React + TS</h1>
-      <Counter />
+      <CounterWithHook />
     </main>
   );
 }
