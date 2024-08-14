b26dc69cc11d3df6edfcd14ce626a2ce9acf49f2
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-14 22:00:19 +0000

    Sección 2: Funciones, retorno y argumentos

--- 01-react-foundation/src/App.tsx
+++ 01-react-foundation/src/App.tsx
@@ -1,13 +1,14 @@
 import {
+  BasicFunctions,
   // BasicTypes,
-  ObjectLiterals
+  // ObjectLiterals
 } from "./typescript";
 
 function App() {
   return (
     <main>
       <h1>Introducción a React + TS</h1>
-      <ObjectLiterals />
+      <BasicFunctions />
     </main>
   );
 }
