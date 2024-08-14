fbf3d3d556904584d6014bae82db7050775008a6
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-14 22:11:33 +0000

    Sección 2: Hook - useState

--- 01-react-foundation/src/App.tsx
+++ 01-react-foundation/src/App.tsx
@@ -1,14 +1,11 @@
-import {
-  BasicFunctions,
-  // BasicTypes,
-  // ObjectLiterals
-} from "./typescript";
+import { Counter } from "./components";
+// import {BasicFunctions, BasicTypes, ObjectLiterals} from "./typescript";
 
 function App() {
   return (
     <main>
       <h1>Introducción a React + TS</h1>
-      <BasicFunctions />
+      <Counter />
     </main>
   );
 }
