4487483338fad0f27d6046de2b7ba5f54e7ba436
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-15 13:06:25 +0000

    Sección 2: Envío y valores del formulario

--- 01-react-foundation/src/components/FormsPage.tsx
+++ 01-react-foundation/src/components/FormsPage.tsx
@@ -6,19 +6,23 @@ type FormInputs = {
 };
 
 export const FormsPage = () => {
-  const { register } = useForm<FormInputs>({
+  const { register, handleSubmit } = useForm<FormInputs>({
     defaultValues: {
       email: "fernando@google.com",
       password: "Abc123"
     }
   });
 
+  const onSubmit = (myForm: FormInputs) => {
+    console.log({ myForm });
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h3>Formularios</h3>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <input type="text" placeholder="Email" {...register("email")} />
+          <input type="text" placeholder="Email" {...register("email", { required: true })} />
           <input type="text" placeholder="Password" {...register("password")} />
           <button type="submit">Ingresar</button>
         </div>
