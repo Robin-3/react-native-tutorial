30e28ea1f49a9f760af4a2d99b4c5cd5de4363f2
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-15 12:10:12 +0000

    Sección 2: Paginación de resultados

--- 01-react-foundation/src/components/UsersPage.tsx
+++ 01-react-foundation/src/components/UsersPage.tsx
@@ -1,10 +1,14 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { ReqResUserListResponse, User } from "../interfaces";
 
-const loadUsers = async (): Promise<User[]> => {
+const loadUsers = async (page: number = 1): Promise<User[]> => {
   try {
-    const { data } = await axios.get<ReqResUserListResponse>("https://reqres.in/api/users");
+    const { data } = await axios.get<ReqResUserListResponse>("https://reqres.in/api/users", {
+      params: {
+        page
+      }
+    });
     return data.data;
   } catch (error) {
     console.log(error);
@@ -14,10 +18,28 @@ const loadUsers = async (): Promise<User[]> => {
 
 export const UsersPage = () => {
   const [userList, setUserList] = useState<User[]>([]);
+  const currentPageRef = useRef<number>(1);
 
   useEffect(() => {
-    loadUsers().then(setUserList);
+    loadUsers(currentPageRef.current).then(setUserList);
   }, []);
+
+  const nextPage = async () => {
+    currentPageRef.current++;
+    const users = await loadUsers(currentPageRef.current);
+    if (users.length > 0) {
+      setUserList(users);
+    } else {
+      currentPageRef.current--;
+    }
+  };
+
+  const prevPage = async () => {
+    if (currentPageRef.current < 1) return;
+    currentPageRef.current--;
+    const users = await loadUsers(currentPageRef.current);
+    setUserList(users);
+  };
 
   return (
     <>
@@ -36,6 +58,10 @@ export const UsersPage = () => {
           ))}
         </tbody>
       </table>
+      <div>
+        <button onClick={prevPage}>Prev</button>
+        <button onClick={nextPage}>Next</button>
+      </div>
     </>
   );
 };
