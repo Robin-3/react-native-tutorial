54a4fa9f0cc1b6ba0f514a0a765b31a51ff55e0d
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-16 17:51:01 +0000

    Sección 4: Crear un contador

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,10 +1,11 @@
 import { SafeAreaView } from "react-native";
-import { HelloWorldScreen } from "./src/presentation/screens/HelloWorldScreen";
+// import { HelloWorldScreen } from "./src/presentation/screens/HelloWorldScreen";
+import { CounterScreen } from "./src/presentation/screens/CounterScreen";
 
 export const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <HelloWorldScreen name="Fernando Herrera" />
+      <CounterScreen />
     </SafeAreaView>
   );
 };
