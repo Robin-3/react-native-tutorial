da9d23ec27aa0a3ac8bb4500ea1a9b5d892e9b30
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-16 21:17:35 +0000

    Sección 4: Iconos en React Native - Android

--- 02-MyFirstApp/src/presentation/screens/CounterM3Screen.tsx
+++ 02-MyFirstApp/src/presentation/screens/CounterM3Screen.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { globalStyles } from "../theme/global.styles";
 import { FAB } from "react-native-paper";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export const CounterM3Screen = () => {
   const [count, setCount] = useState(10);
@@ -9,6 +10,7 @@ export const CounterM3Screen = () => {
   return (
     <View style={globalStyles.centerCntainer}>
       <Text style={globalStyles.title}>{count}</Text>
+      <Icon name="accessibility-outline" size={35} />
       <FAB
         label="+1"
         onPress={() => setCount(count + 1)}
