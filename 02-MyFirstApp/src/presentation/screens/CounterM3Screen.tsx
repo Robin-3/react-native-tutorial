a1370760fe35aec7922c83c8306485cd9b799435
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-16 21:29:45 +0000

    Sección 4: Configurar iconos globales

--- 02-MyFirstApp/src/presentation/screens/CounterM3Screen.tsx
+++ 02-MyFirstApp/src/presentation/screens/CounterM3Screen.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Text, View } from "react-native";
 import { globalStyles } from "../theme/global.styles";
 import { FAB } from "react-native-paper";
-import Icon from "react-native-vector-icons/Ionicons";
 
 export const CounterM3Screen = () => {
   const [count, setCount] = useState(10);
@@ -10,12 +9,11 @@ export const CounterM3Screen = () => {
   return (
     <View style={globalStyles.centerCntainer}>
       <Text style={globalStyles.title}>{count}</Text>
-      <Icon name="accessibility-outline" size={35} />
       <FAB
-        label="+1"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
         style={globalStyles.fab}
+        icon="add-outline"
       />
     </View>
   );
