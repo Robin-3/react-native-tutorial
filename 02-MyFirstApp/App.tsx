a68e603302293a6f05c044365b77c076d4bb440f
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-16 22:44:35 +0000

    Sección 5: Height, Width porcentual y dimensiones de la pantalla

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -4,7 +4,8 @@ import { SafeAreaView } from "react-native";
 import { PaperProvider } from "react-native-paper";
 // import { CounterM3Screen } from "./src/presentation/screens/CounterM3Screen";
 import IonIcons from "react-native-vector-icons/Ionicons";
-import { BoxObjectModelScreeen } from "./src/presentation/screens/BoxObjectModelScreeen";
+// import { BoxObjectModelScreeen } from "./src/presentation/screens/BoxObjectModelScreeen";
+import { DimensionScreen } from "./src/presentation/screens/DimensionScreen";
 
 export const App = () => {
   return (
@@ -14,7 +15,7 @@ export const App = () => {
       }}
     >
       <SafeAreaView style={{ flex: 1 }}>
-        <BoxObjectModelScreeen />
+        <DimensionScreen />
       </SafeAreaView>
     </PaperProvider>
   );
