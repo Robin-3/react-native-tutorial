09f8c848151ce54d3f898354a6869aaf10089aae
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-16 22:05:19 +0000

    Sección 5: Padding, Margin, Border, Width y Height

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -2,8 +2,9 @@ import { SafeAreaView } from "react-native";
 // import { HelloWorldScreen } from "./src/presentation/screens/HelloWorldScreen";
 // import { CounterScreen } from "./src/presentation/screens/CounterScreen";
 import { PaperProvider } from "react-native-paper";
-import { CounterM3Screen } from "./src/presentation/screens/CounterM3Screen";
+// import { CounterM3Screen } from "./src/presentation/screens/CounterM3Screen";
 import IonIcons from "react-native-vector-icons/Ionicons";
+import { BoxObjectModelScreeen } from "./src/presentation/screens/BoxObjectModelScreeen";
 
 export const App = () => {
   return (
@@ -13,7 +14,7 @@ export const App = () => {
       }}
     >
       <SafeAreaView style={{ flex: 1 }}>
-        <CounterM3Screen />
+        <BoxObjectModelScreeen />
       </SafeAreaView>
     </PaperProvider>
   );
