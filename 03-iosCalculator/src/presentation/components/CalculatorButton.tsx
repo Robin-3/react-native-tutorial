455b3375a975457e832ef850618b1332459c32fc
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-17 21:56:06 +0000

    Sección 6: Completar botón de la calculadora

--- 03-iosCalculator/src/presentation/components/CalculatorButton.tsx
+++ 03-iosCalculator/src/presentation/components/CalculatorButton.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text } from "react-native";
+import { GestureResponderEvent, Pressable, Text } from "react-native";
 import { colors, styles } from "../../config/theme/app-theme";
 
 interface Props {
@@ -6,21 +6,26 @@ interface Props {
   color?: string;
   doubleSize?: boolean;
   colorTextInverse?: boolean;
+  onPress: (event: GestureResponderEvent) => void;
 }
 
 export const CalculatorButton = ({
   label,
   color = colors.darkGray,
   doubleSize = false,
-  colorTextInverse = false
+  colorTextInverse = false,
+  onPress
 }: Props) => {
   return (
-    <Pressable style={({ pressed }) => ({
-      ...styles.button,
-      backgroundColor: color,
-      width: doubleSize ? 180 : 80,
-      opacity: pressed ? 0.8 : 1
-    })}>
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => ({
+        ...styles.button,
+        backgroundColor: color,
+        width: doubleSize ? 180 : 80,
+        opacity: pressed ? 0.8 : 1
+      })}
+    >
       <Text
         style={{
           ...styles.buttonText,
