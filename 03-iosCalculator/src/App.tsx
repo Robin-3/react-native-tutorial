55d71d83e939fe09a871a02800e94be043686438
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-17 18:10:48 +0000

    Sección 6: Estructura inicial

---
+++ 03-iosCalculator/src/App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar, View } from 'react-native';
+import { CalculatorScreen } from "./presentation/screens/CalculatorScreen";
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={"black"}
+      />
+      <CalculatorScreen />
+    </View>
+  );
+};
+
+export default App;
