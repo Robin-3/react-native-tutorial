e6c0b98d0e3a85df4d61680ad03ed79d8c9bd6e7
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-17 18:27:15 +0000

    Sección 6: Diseño de pantalla y estilos globales

--- 03-iosCalculator/src/presentation/screens/CalculatorScreen.tsx
+++ 03-iosCalculator/src/presentation/screens/CalculatorScreen.tsx
@@ -1,7 +1,14 @@
+import { Text } from "react-native";
 import { View } from "react-native";
+import { styles } from "../../config/theme/app-theme";
 
 export const CalculatorScreen = () => {
   return (
-    <View></View>
+    <View style={styles.calculatorContainer}>
+      <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
+        <Text style={styles.mainResult}>1500</Text>
+        <Text style={styles.subResult}>15</Text>
+      </View>
+    </View>
   );
 };
