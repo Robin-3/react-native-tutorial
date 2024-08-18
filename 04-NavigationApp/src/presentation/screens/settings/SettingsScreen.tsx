ea4d3e24696fa0c2a724f2d790d628f350a843a8
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-18 20:30:39 +0000

    Sección 7: Archivos y directorios del proyecto

---
+++ 04-NavigationApp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export const SettingsScreen = () => {
+  return (
+    <View></View>
+  );
+};
---
+++ 04-NavigationApp/src/presentation/screens/tabs/Tab1Screen.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export const Tab1Screen = () => {
+  return (
+    <View></View>
+  );
+};
