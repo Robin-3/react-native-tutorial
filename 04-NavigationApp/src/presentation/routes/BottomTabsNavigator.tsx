bc0f45d7462cd8be4a470ad4e4def966946f6672
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-18 23:22:07 +0000

    Sección 8: Personalizando el BottomTabNavigator

--- 04-NavigationApp/src/presentation/routes/BottomTabsNavigator.tsx
+++ 04-NavigationApp/src/presentation/routes/BottomTabsNavigator.tsx
@@ -2,15 +2,56 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Tab1Screen } from "../screens/tabs/Tab1Screen";
 import { Tab2Screen } from "../screens/tabs/Tab2Screen";
 import { Tab3Screen } from "../screens/tabs/Tab3Screen";
+import { globalColors } from "../theme/theme";
+import { Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabsNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1" component={Tab1Screen} />
-      <Tab.Screen name="Tab2" component={Tab2Screen} />
-      <Tab.Screen name="Tab3" component={Tab3Screen} />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: globalColors.background
+      }}
+      screenOptions={{
+        tabBarLabelStyle: {
+          marginBottom: 5
+        },
+        headerStyle: {
+          elevation: 0,
+          borderColor: "transparent",
+          shadowColor: "transparent"
+        },
+        tabBarStyle: {
+          elevation: 0,
+          borderTopWidth: 0
+        }
+      }}
+    >
+      <Tab.Screen
+        name="Tab1"
+        options={{
+          title: "Tab 1",
+          tabBarIcon: ({ color }) => <Text style={{ color }}>"Tab 1"</Text>
+        }}
+        component={Tab1Screen}
+      />
+      <Tab.Screen
+        name="Tab2"
+        options={{
+          title: "Tab 2",
+          tabBarIcon: ({ color }) => <Text style={{ color }}>"Tab 2"</Text>
+        }}
+        component={Tab2Screen}
+      />
+      <Tab.Screen
+        name="Tab3"
+        options={{
+          title: "Tab 3",
+          tabBarIcon: ({ color }) => <Text style={{ color }}>"Tab 3"</Text>
+        }}
+        component={Tab3Screen}
+      />
     </Tab.Navigator>
   );
 };
