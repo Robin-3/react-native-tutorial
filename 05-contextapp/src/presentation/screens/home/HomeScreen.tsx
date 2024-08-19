4dffec9fcd985f13327f30f908c46592620a2c3b
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-19 16:12:12 +0000

    Sección 9: Preparación de la aplicación y tarea

---
+++ 05-contextapp/src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import { styles } from "../../../config";
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.container} />
+  );
+};
---
+++ 05-contextapp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import { styles } from "../../../config";
+
+export const SettingsScreen = () => {
+  return (
+    <View style={styles.container} />
+  );
+};
