b6bfaccb1257854566c283ee270ea14a44a86dbd
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-19 16:29:08 +0000

    Tarea

--- 05-contextapp/src/Main.tsx
+++ 05-contextapp/src/Main.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { BottomTabsNavigation } from "./presentation/navigators/BottomTabsNavigation";
 
 export const Main = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
+    <NavigationContainer>
+      <BottomTabsNavigation />
+    </NavigationContainer>
   );
 };
