f575d07b4854e4ce7318077f878a1900d70430e6
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-20 14:51:36 +0000

    Sección 11: Información de la película por ID

--- 06-moviesApp/src/presentation/screens/details/DetailsScreen.tsx
+++ 06-moviesApp/src/presentation/screens/details/DetailsScreen.tsx
@@ -1,6 +1,12 @@
+import { type StackScreenProps } from "@react-navigation/stack";
 import { View } from "react-native";
+import { type RootStackParams } from "../../navigation/Navigation";
 
-export const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, "Details"> { }
+
+export const DetailsScreen = ({ route }: Props) => {
+  const { movieId } = route.params;
+
   return (
     <View />
   );
