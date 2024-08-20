9c9b2758d88d30ac4a91f0fcff9c48f5f5f61dfe
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-20 12:25:06 +0000

    Sección 11: Carrusel de películas don FlatList

--- 06-moviesApp/src/presentation/components/movies/MoviePoster.tsx
+++ 06-moviesApp/src/presentation/components/movies/MoviePoster.tsx
@@ -18,13 +18,13 @@ export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
       style={({ pressed }) => ({
         width,
         height,
-        marginHorizontal: 10,
+        marginHorizontal: 2,
         paddingBottom: 20,
-        paddingHorizontal: 10,
+        paddingHorizontal: 7,
         opacity: pressed ? 0.9 : 1
       })}
     >
-      <View style={{ ...styles.imageContainer, height, width }}>
+      <View style={styles.imageContainer}>
         <Image
           style={styles.image}
           source={{ uri: movie.poster }}
--- 06-moviesApp/src/presentation/screens/home/HomeScreen.tsx
+++ 06-moviesApp/src/presentation/screens/home/HomeScreen.tsx
@@ -3,10 +3,17 @@ import { useMovies } from "../../hooks/useMovies";
 import { ScrollView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { PosterCarrousel } from "../../components/movies/PosterCarrousel";
+import { HorizontalCarrousel } from "../../components/movies/HorizontalCarrousel";
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
-  const { isLoading, nowPlaying } = useMovies();
+  const {
+    isLoading,
+    nowPlaying,
+    popular,
+    topRated,
+    upcoming
+  } = useMovies();
 
   if (isLoading) {
     return <Text>Cargando...</Text>;
@@ -21,6 +28,9 @@ export const HomeScreen = () => {
         }}
       >
         <PosterCarrousel movies={nowPlaying} />
+        <HorizontalCarrousel movies={popular} title="Populares" />
+        <HorizontalCarrousel movies={topRated} title="Mejor calificadas" />
+        <HorizontalCarrousel movies={upcoming} title="Próximamente" />
       </View>
     </ScrollView>
   );
