e601f6e8b516a5c06c2de17f675471ab12e3a2f5
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-20 22:34:43 +0000

    Sección 12: Animated API

--- 07-componentsApp/src/presentation/navigation/Navigation.tsx
+++ 07-componentsApp/src/presentation/navigation/Navigation.tsx
@@ -1,12 +1,14 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen } from "../screens/home/HomeScreen";
+import { Animation101Screen } from "../screens/animations/Animation101Screen";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export const Navigation = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="Home" component={HomeScreen} />
+      <Screen name="HomeScreen" component={HomeScreen} />
+      <Screen name="Animation101Screen" component={Animation101Screen} />
     </Navigator>
   );
 };
---
+++ 07-componentsApp/src/presentation/screens/animations/Animation101Screen.tsx
@@ -0,0 +1,53 @@
+import { Animated, Pressable, StyleSheet, Text, View } from "react-native";
+import { colors } from "../../../config/theme/theme";
+import { useRef } from "react";
+
+export const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  };
+
+  return (
+    <View style={style.container}>
+      <Animated.View
+        style={{
+          ...style.purpleBox,
+          opacity: animatedOpacity
+        }}
+      />
+      <Pressable onPress={fadeIn} style={{ marginTop: 10 }}>
+        <Text>FadeIn</Text>
+      </Pressable>
+      <Pressable onPress={fadeOut} style={{ marginTop: 10 }}>
+        <Text>FadeOut</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  purpleBox: {
+    backgroundColor: colors.primary,
+    width: 150,
+    height: 150
+  }
+});
