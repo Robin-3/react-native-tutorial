3b2a68d116940538fa9649e5fca1b2984c7836cc
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-21 22:49:31 +0000

    Sección 14: Interfaces de PokeAPI

--- 08-pokedex/src/actions/pokemons/get-pokemons.ts
+++ 08-pokedex/src/actions/pokemons/get-pokemons.ts
@@ -1,10 +1,21 @@
 import { pokeApi } from "../../config/api/pokeApi";
 import { type Pokemon } from "../../domain/entities/pokemon";
+import {
+  type PokeAPIPokemonResponse,
+  type PokeAPIPaginatedResponse
+} from "../../infrastructure/interfaces/pokeapi.interfaces";
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (page: number, limit: number = 20): Promise<Pokemon[]> => {
   try {
-    const url = "/pokemon";
-    const { data } = await pokeApi.get(url);
+    const offset = page * limit; // basado en cuantos elementos estoy pidiendo
+    const url = `/pokemon?offset=${offset}&limit=${limit}`;
+    const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+    const pokemonPromises = data.results.map((info) => {
+      return pokeApi.get<PokeAPIPokemonResponse>(info.url);
+    });
+
+    const pokeApiPokemons = await Promise.all(pokemonPromises);
 
     return [];
   } catch (error) {
