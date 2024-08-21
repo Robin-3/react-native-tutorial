f47241e0d9b666137bd07b6e81309dcddefde2e1
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-21 21:10:39 +0000

    Sección 14: Stack Navigator

---
+++ 08-pokedex/src/presentation/Navigator/StackNavigator.tsx
@@ -0,0 +1,22 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen } from "../screens/home/HomeScreen";
+import { PokemonScreen } from "../screens/pokemon/PokemonScreen";
+import { SearchScreen } from "../screens/search/SearchScreen";
+
+export type RootStackParams = {
+  Home: undefined;
+  Pokemon: { pokemonId: number; };
+  Search: undefined;
+};
+
+const { Navigator, Screen } = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Navigator>
+      <Screen name="Home" component={HomeScreen} />
+      <Screen name="Pokemon" component={PokemonScreen} />
+      <Screen name="Search" component={SearchScreen} />
+    </Navigator>
+  );
+};
