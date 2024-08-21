e93bc82435eb58f6e1fd6373a18d42908bc80cca
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-21 12:07:10 +0000

    Sección 12: Componente - Switch

--- 07-componentsApp/src/presentation/screens/switches/SwitchScreen.tsx
+++ 07-componentsApp/src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import { CustomView } from "../../components/ui/CustomView";
+import { Card } from "../../components/ui/Card";
+import { CustomSwitch } from "../../components/ui/CustomSwitch";
 
 export const SwitchScreen = () => {
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true
+  });
+
   return (
     <CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
+      <Card>
+        <CustomSwitch
+          isOn={state.isActive}
+          onChange={(value) => setState({ ...state, isActive: value })}
+          text="Activo"
+        />
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={(value) => setState({ ...state, isHungry: value })}
+          text="Activo"
+        />
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={(value) => setState({ ...state, isHappy: value })}
+          text="Activo"
+        />
+      </Card>
     </CustomView >
   );
 };
