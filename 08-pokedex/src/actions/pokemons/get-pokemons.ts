5289a5fda78a3025e426f10d852e7ff610a2744a
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-21 22:07:40 +0000

    Sección 14: TanStack Query - Peticiones y caché

--- 08-pokedex/src/actions/pokemons/get-pokemons.ts
+++ 08-pokedex/src/actions/pokemons/get-pokemons.ts
@@ -1,7 +1,7 @@
 import { pokeApi } from "../../config/api/pokeApi";
 import { type Pokemon } from "../../domain/entities/pokemon";
 
-export const getPokemon = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (): Promise<Pokemon[]> => {
   try {
     const url = "/pokemon";
     const { data } = await pokeApi.get(url);
--- 08-pokedex/src/presentation/screens/home/HomeScreen.tsx
+++ 08-pokedex/src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import { View } from "react-native";
+import { getPokemons } from "../../../actions/pokemons";
+import { ActivityIndicator } from "react-native-paper";
 
 export const HomeScreen = () => {
+  const { isLoading, data } = useQuery({
+    queryKey: ["pokemons"],
+    queryFn: () => getPokemons(),
+    staleTime: 1000 * 60 * 60 //60 min
+  });
+
   return (
-    <View />
+    <View>
+      {isLoading && <ActivityIndicator />}
+    </View>
   );
 };
