a8c44e79b13f95a78b489f80e2a129b4181a0a7f
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-24 17:35:57 +0000

    Sección 15: useDebouncedValue - CustomHook

--- 08-pokedex/src/presentation/screens/search/SearchScreen.tsx
+++ 08-pokedex/src/presentation/screens/search/SearchScreen.tsx
@@ -2,15 +2,16 @@ import { FlatList, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { globalTheme } from "../../../config/theme/global-theme";
 import { ActivityIndicator, TextInput } from "react-native-paper";
-import { type Pokemon } from "../../../domain/entities/pokemon";
 import { PokemonCard } from "../../components/pokemons/PokemonCard";
 import { useQuery } from "@tanstack/react-query";
 import { getPokemonNmaesWithId, getPokemonsByIds } from "../../../actions/pokemons";
 import { useMemo, useState } from "react";
 import { FullScreenLoader } from "../../components/ui/FullScreenLoader";
+import { useDebouncedValue } from "../../hooks/useDebouncedValue";
 
 export const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const debouncedValue = useDebouncedValue(term);
   const { top } = useSafeAreaInsets();
   const { isLoading, data: pokemonNameList = [] } = useQuery({
     queryKey: ["pokemons", "all"],
@@ -19,14 +20,14 @@ export const SearchScreen = () => {
   });
 
   const pokemonNameIdList = useMemo(() => {
-    if (!isNaN(Number(term))) {
-      const pokemon = pokemonNameList.find((pokemon) => pokemon.id === Number(term));
+    if (!isNaN(Number(debouncedValue))) {
+      const pokemon = pokemonNameList.find((pokemon) => pokemon.id === Number(debouncedValue));
       return pokemon ? [pokemon] : [];
     }
-    if (term.length === 0) return [];
-    if (term.length < 3) return [];
-    return pokemonNameList.filter((pokemon) => pokemon.name.includes(term.toLocaleLowerCase()));
-  }, [term]);
+    if (debouncedValue.length === 0) return [];
+    if (debouncedValue.length < 3) return [];
+    return pokemonNameList.filter((pokemon) => pokemon.name.includes(debouncedValue.toLocaleLowerCase()));
+  }, [debouncedValue]);
 
   const idList = pokemonNameIdList.map((pokemon) => pokemon.id);
   const { isLoading: isLoadingPokemons, data: pokemons = [] } = useQuery({
