82c33a02c5b3b3eab6084a3e572809f884d8798c
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-24 16:53:48 +0000

    Sección 15: Filtrar listado por id y nombre

--- 08-pokedex/src/presentation/screens/search/SearchScreen.tsx
+++ 08-pokedex/src/presentation/screens/search/SearchScreen.tsx
@@ -1,18 +1,30 @@
 import { FlatList, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { globalTheme } from "../../../config/theme/global-theme";
-import { ActivityIndicator, Text, TextInput } from "react-native-paper";
+import { ActivityIndicator, TextInput } from "react-native-paper";
 import { type Pokemon } from "../../../domain/entities/pokemon";
 import { PokemonCard } from "../../components/pokemons/PokemonCard";
 import { useQuery } from "@tanstack/react-query";
 import { getPokemonNmaesWithId } from "../../../actions/pokemons";
+import { useMemo, useState } from "react";
 
 export const SearchScreen = () => {
+  const [term, setTerm] = useState("");
   const { top } = useSafeAreaInsets();
-  const { data: PokemonNameList = [] } = useQuery({
+  const { data: pokemonNameList = [] } = useQuery({
     queryKey: ["pokemons", "all"],
     queryFn: () => getPokemonNmaesWithId()
   });
+
+  const pokemonNameIdList = useMemo(() => {
+    if (!isNaN(Number(term))) {
+      const pokemon = pokemonNameList.find((pokemon) => pokemon.id === Number(term));
+      return pokemon ? [pokemon] : [];
+    }
+    if (term.length === 0) return [];
+    if (term.length < 3) return [];
+    return pokemonNameList.filter((pokemon) => pokemon.name.includes(term.toLocaleLowerCase()));
+  }, [term]);
 
   return (
     <View style={[globalTheme.globalMargin, { padding: top + 10 }]}>
@@ -21,6 +33,8 @@ export const SearchScreen = () => {
         mode="flat"
         autoFocus
         autoCorrect={false}
+        value={term}
+        onChangeText={setTerm}
       />
       <ActivityIndicator style={{ paddingTop: 20 }} />
       <FlatList
