61aac6ee0a15ddf81c9bad8c4794567af7cedfe0
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-25 13:16:13 +0000

    Sección 16: Rutas y StackNavigator

---
+++ 09-mapsapp/src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,28 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoadingScreen } from "../screens/loading/LoadingScreen";
+import { MapScreen } from "../screens/maps/MapScreen";
+import { PermissionsScreen } from "../screens/premissions/PermissionsScreen";
+
+export type RootStackParams = {
+  Loading: undefined;
+  Permissions: undefined;
+  Maps: undefined;
+};
+
+const { Navigator, Screen } = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Navigator
+      initialRouteName="Loading"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: "white" }
+      }}
+    >
+      <Screen name="Loading" component={LoadingScreen} />
+      <Screen name="Maps" component={MapScreen} />
+      <Screen name="Permissions" component={PermissionsScreen} />
+    </Navigator>
+  );
+};
