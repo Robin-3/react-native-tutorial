76c49aa1d3ebc425cac704ca43efcb82851a967c
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-25 17:59:49 +0000

    Sección 16: PermissionStore - Zustand

--- 09-mapsapp/src/actions/permissions/location.ts
+++ 09-mapsapp/src/actions/permissions/location.ts
@@ -15,6 +15,7 @@ export const requestLocationPermission = async (): Promise<PermissionStatus> => 
 
   if (status === "blocked") {
     await openSettings();
+    return await checkLocationPermission();
   }
 
   const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
