39f99c8e0d06cf0b19ba3f0cb3efeea8137086de
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-27 20:51:36 +0000

    Sección 17: Mover la cámara a las coordenadas del usuario

--- 09-mapsapp/src/presentation/components/maps/Map.tsx
+++ 09-mapsapp/src/presentation/components/maps/Map.tsx
@@ -2,6 +2,8 @@ import { Platform } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { type Location } from "../../../infrastructure/interfaces/location";
 import { FAB } from "../ui/FAB";
+import { useRef } from "react";
+import { useLocationStore } from "../../store/location/useLocationStore";
 
 interface Props {
   showUserLocation?: boolean;
@@ -9,21 +11,37 @@ interface Props {
 }
 
 export const Map = ({ showUserLocation = true, initialLocation }: Props) => {
+  const { getLocation } = useLocationStore();
+  const mapRef = useRef<MapView>();
+  const cameraLocation = useRef<Location>(initialLocation);
+
+  const moveCameraToLocation = (location: Location) => {
+    if (!mapRef.current) return;
+    mapRef.current.animateCamera({ center: location });
+  };
+
+  const moveToCurrentLocation = async () => {
+    const location = await getLocation();
+    if (!location) return;
+    moveCameraToLocation(location);
+  };
+
   return (
     <>
       <MapView
+        ref={(map) => mapRef.current = map!}
         showsUserLocation={showUserLocation}
         provider={Platform.OS === "ios" ? undefined : PROVIDER_GOOGLE}
         style={{ flex: 1 }}
         region={{
-          ...initialLocation,
+          ...cameraLocation.current,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121
         }}
       />
       <FAB
         iconName="compass-outline"
-        onPress={() => { }}
+        onPress={moveToCurrentLocation}
         style={{ bottom: 20, right: 20 }}
       />
     </>
