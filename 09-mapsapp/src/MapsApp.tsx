6fe6c0ca25c1e039855c80f7e05fae63a6a13d6b
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-27 18:34:45 +0000

    Sección 17: Configurar IOS y Android

--- 09-mapsapp/src/MapsApp.tsx
+++ 09-mapsapp/src/MapsApp.tsx
@@ -2,6 +2,9 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { StackNavigator } from "./presentation/navigation/StackNavigator";
 import { PermissionsCheker } from "./presentation/providers/PermissionsChecker";
+import { enableLatestRenderer } from "react-native-maps";
+
+enableLatestRenderer();
 
 export const MapsApp = () => {
   return (
--- 09-mapsapp/src/presentation/screens/maps/MapScreen.tsx
+++ 09-mapsapp/src/presentation/screens/maps/MapScreen.tsx
@@ -1,7 +1,24 @@
-import { View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export const MapScreen = () => {
   return (
-    <View />
+    <View style={styles.container}>
+      <MapView
+        provider={Platform.OS === "ios" ? undefined : PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121
+        }}
+      ></MapView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { ...StyleSheet.absoluteFillObject },
+  map: { ...StyleSheet.absoluteFillObject }
+});
