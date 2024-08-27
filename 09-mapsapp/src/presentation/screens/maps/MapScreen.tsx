689f2759a5a7e2ac887e207ad74f316517e81213
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-27 20:24:29 +0000

    Sección 17: Store - Información de la ubicación

--- 09-mapsapp/src/presentation/screens/maps/MapScreen.tsx
+++ 09-mapsapp/src/presentation/screens/maps/MapScreen.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, View } from "react-native";
 import { Map } from "../../components/maps/Map";
+import { useLocationStore } from "../../store/location/useLocationStore";
+import { LoadingScreen } from "../loading/LoadingScreen";
+import { useEffect } from "react";
 
 export const MapScreen = () => {
+  const { lastKnownLocation, getLocation } = useLocationStore();
+
+  useEffect(() => {
+    if (!lastKnownLocation) getLocation();
+  }, []);
+
+  if (!lastKnownLocation) return <LoadingScreen />;
+
   return (
     <View style={styles.container}>
-      {/* <Map /> */}
+      <Map initialLocation={lastKnownLocation} />
     </View>
   );
 };
